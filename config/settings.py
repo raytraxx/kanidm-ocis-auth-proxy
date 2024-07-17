8802fca1cbf5a595f511e024be0644ee7ce6354e
Alejandro Avila <avila.alejandro@outlook.es>
2024-07-17 12:10:42 +0200

    parametrize client_id

--- config/settings.py
+++ config/settings.py
@@ -3,9 +3,12 @@ import os
 SECRET_KEY = os.getenv("SECRET_KEY", "LoremIpsum123")
 DEBUG = os.getenv("FLASK_DEBUG", "false").lower() == "true"
 
+CLIENT_ID = os.getenv("CLIENT_ID")
+
 SERVER_NAME = os.getenv(
     "SERVER_NAME", "localhost:{0}".format(os.getenv("PORT", "8000"))
 )
 
-BASE_URL = os.getenv("BASE_URL", "http://localhost:{0}".format(os.getenv("PORT", "8000")))
+SCHEME = os.getenv("SCHEME", "http" if SERVER_NAME.startswith("localhost") else "https")
+BASE_URL = f"{SCHEME}://{SERVER_NAME}"
 IDM_BASE_URL = os.getenv("IDM_BASE_URL", "http://localhost:{0}".format(os.getenv("PORT", "8000")))
--- proxy/routes.py
+++ proxy/routes.py
@@ -10,36 +10,13 @@ from config import settings
 api = Blueprint('api', __name__)
 
 
-@api.get("/.well-known/webfinger")
-def webfinger():
-    resource = request.args.get("resource")
-    return {
-        "subject": resource,
-        "links": [{
-            "rel": "http://openid.net/specs/connect/1.0/issuer",
-            "href": f"{settings.BASE_URL}/oauth2/openid/ocis"
-        }]
-    }
-
-
-@api.get("/oauth2/openid/ocis/.well-known/openid-configuration")
-def openid_configuration():
-    r = requests.get(f"{settings.IDM_BASE_URL}/oauth2/openid/ocis/.well-known/openid-configuration")
-    data = r.json()
-
-    data["authorization_endpoint"] = f"{settings.BASE_URL}/ui/oauth2"
-    data["token_endpoint"] = f"{settings.BASE_URL}/oauth2/token"
-
-    return data
-
-
 @api.get("/redirect-request")
 def ui_oauth2():
     args = request.args.to_dict()
 
     session["original_redirect_uri"] = args["redirect_uri"]
 
-    args['client_id'] = "ocis-desktop"
+    args['client_id'] = settings.CLIENT_ID
     args['redirect_uri'] = f"{settings.BASE_URL}/redirect-response"
 
     query = urllib.parse.urlencode(args)
@@ -63,7 +40,8 @@ def oauth2_token():
 
     if "redirect_uri" in form.keys() and ("localhost" in form["redirect_uri"] or "127.0.0.1" in form["redirect_uri"]):
         form["redirect_uri"] = f"{settings.BASE_URL}/redirect-response"
-        headers["Authorization"] = b"Basic " + base64.b64encode(b"ocis-desktop:eDTxBa9aVQaCGPEkqQPUYf4vELWhqQpsP7pA8jpFrrryVcMG")
+        headers["Authorization"] = b"Basic " + base64.b64encode(
+            bytes(settings.CLIENT_ID, "utf-8") + b":eDTxBa9aVQaCGPEkqQPUYf4vELWhqQpsP7pA8jpFrrryVcMG")
         headers["Host"] = "auth.alejandroavila.com"
         del headers["Content-Length"]
 
